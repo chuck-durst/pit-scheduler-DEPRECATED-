76ec60b161ff8237b179046b63abc8068cea02bc
durst_c <charles.durst@payintech.com>
2016-09-15 10:54:17 +0200

    first commit

---
+++ js/pit-scheduler.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function ($) {
+    var i18n = {
+
+    };
+    
+    $.fn.pitScheduler = function (options){
+
+      /*  var $scheduler = $(this);
+
+        var settings = $.extend({
+            date: {
+                current: moment(),
+                selected: moment()
+            },
+            ui: {
+
+            }
+        }, options);
+
+        /!********* Initialization *********!/
+        var initializeCalendar = function () {
+            var daysNumber = settings.date.selected.daysInMonth(),
+                $container =    '<div class="col-lg-12 pts-container text-center">' +
+                                '<button class="btn btn-primary pts-btn-previous-day">Previous day</button>' +
+                                '<button class="btn btn-primary pts-btn-next-day">Next day</button>' +
+                                '</div>';
+
+            $scheduler.addClass('row');
+            $scheduler.append($container);
+            console.log(daysNumber);
+        };
+
+        initializeCalendar();
+
+        return $scheduler;*/
+    };
+}(jQuery));
